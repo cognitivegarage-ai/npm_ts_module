2484ab146e155030e9c6668ae0a1e601d8a3a0df
shivansh-cg <shivansh@cognitivegarage.ai>
2023-03-16 13:08:22 +0530

    build

---
+++ dist/index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+function sum(...nums) {
+    let i = 0, total = 0;
+    for (; i < nums.length; i++)
+        total += nums[i];
+    return total;
+}
+function substract(...nums) {
+    let i = 0, total = nums[i++] | 0;
+    for (; i < nums.length; i++)
+        total -= nums[i];
+    return total;
+}
+function average(...nums) {
+    let i = 0, len = nums.length, total = 0;
+    for (; i < len; i++)
+        total += nums[i];
+    return total / len;
+}
+function special() {
+    return Math.random() * 1000;
+}
+
+exports.average = average;
+exports.special = special;
+exports.substract = substract;
+exports.sum = sum;
